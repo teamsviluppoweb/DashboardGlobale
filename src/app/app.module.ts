42ebd0e48d2b87f37c8964359bf25d9c77009295
Rabbithols <roberturturica@gmail.com>
2019-12-17 15:26:16 +0100

    feat: implementato home apge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {SharedModule} from "./shared/modules/shared.module";
+import { HomeComponent } from './componenets/home/home.component';
+import {MaterialModule} from './shared/modules/material.module';
+import {FooterComponent} from './componenets/footer/footer.component';
+import { TipologiaConcorsoComponent } from './componenets/tipologia-concorso/tipologia-concorso.component';
+import { MenuSidenavComponent } from './componenets/menu-sidenav/menu-sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    TipologiaConcorsoComponent,
+    MenuSidenavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
